5e37f43ac748fc84ef1e93d2b9a3307a9e63646f
Þrãtěęk Ærýà <40314403+prateekarya100@users.noreply.github.com>
2024-11-03 19:20:57 +0530

    loans microservices commit

--- src/main/java/com/tomcat/Loans/dto/ErrorResponseDto.java
+++ src/main/java/com/tomcat/Loans/dto/ErrorResponseDto.java
@@ -1,5 +1,6 @@
 package com.tomcat.Loans.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import org.springframework.http.HttpStatus;
 
@@ -7,9 +8,28 @@ import java.time.LocalDateTime;
 
 @Setter @Getter @ToString
 @AllArgsConstructor @NoArgsConstructor
+@Schema(
+        name = "ErrorResponse",
+        description = "EazyBank restapi endpoints error response"
+)
 public class ErrorResponseDto {
+    @Schema(
+            description = "hold api path where error occur"
+    )
     private String apiPath;
+
+    @Schema(
+            description = "hold error code due to which error occur"
+    )
     private HttpStatus errorCode;
+
+    @Schema(
+            description = "hold server response message"
+    )
     private String serverResponse;
+
+    @Schema(
+            description = "hold timestamp when error occur"
+    )
     private LocalDateTime errorTime;
 }
